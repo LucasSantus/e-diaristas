cb555cdf22f66e120933325fe623f3d5bbe0fc3f
Lucas Santus <leos9877@gmail.com>
2021-06-10 14:37:56 -0300

    implementando front-end

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,7 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@styles/Home.module.css'
+import Head from "next/head";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 
 export default function Home() {
-  return <div> </div>;
+  return (
+    <div>
+      <SafeEnvironment />
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais em sua localidade"
+        }
+      />
+    </div>
+  );
 }
---
+++ ediaristas/src/ui/components/surfaces/Header/Header.tsx
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import React from "react";
+
+const Header: React.FC = () => {
+  return <div></div>;
+};
+
+export default Header;
