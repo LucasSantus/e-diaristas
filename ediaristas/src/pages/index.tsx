42779012fece2c2a45b3ae10986badcbded5c0ae
Lucas Santus <leos9877@gmail.com>
2021-06-14 16:57:52 -0300

    adicionando hooks

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -2,10 +2,13 @@ import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironm
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask"
-import { Button, Typography } from '@material-ui/core';
-import { FormElementsContainer } from 'ui/styles/pages/index.style';
+import { Button, Typography, Container } from '@material-ui/core';
+import { FormElementsContainer, ProfissionaisPaper, ProfissionaisContainer } from 'ui/styles/pages/index.style';
+import useIndex from 'data/hooks/pages/useIndex.page'
 
 export default function Home() {
+  const { cep, setCep } = useIndex();
+
   return (
     <div>
       <SafeEnvironment />
@@ -15,31 +18,78 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais em sua localidade"
         }
       />
-      <FormElementsContainer>
-        <TextFieldMask 
-          mask={'99.999-999'}
-          label={'Digite seu CEP:'}
-          fullWidth
-          variant={'outlined'}
-        />
 
-        <Typography color={'error'}>CEP Inválido</Typography>
-        
-        <Button
-          variant={'contained'}
-          color={'secondary'}
-          sx={{ width: '220px' }}
-        >
-          Buscar
-        </Button>
-      </FormElementsContainer>
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask 
+            mask={'99.999-999'}
+            label={'Digite seu CEP:'}
+            fullWidth
+            variant={'outlined'}
+            value={cep}
+            onChange={(event) => setCep(event.target.value)}
+          />
 
-      <UserInformation
-        name={"Lucas Santus"}
-        picture={"https://github.com/lucasSantus.png"}
-        rating={4}
-        description={"São Paulo"}
-      />
+          {cep}
+
+          <Typography color={'error'}>CEP Inválido</Typography>
+          
+          <Button
+            variant={'contained'}
+            color={'secondary'}
+            sx={{ width: '220px' }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={"Lucas Santus"}
+              picture={"https://github.com/lucasSantus.png"}
+              rating={4}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Lucas Santus"}
+              picture={"https://github.com/lucasSantus.png"}
+              rating={4}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Lucas Santus"}
+              picture={"https://github.com/lucasSantus.png"}
+              rating={4}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Lucas Santus"}
+              picture={"https://github.com/lucasSantus.png"}
+              rating={4}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Lucas Santus"}
+              picture={"https://github.com/lucasSantus.png"}
+              rating={4}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Lucas Santus"}
+              picture={"https://github.com/lucasSantus.png"}
+              rating={4}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Lucas Santus"}
+              picture={"https://github.com/lucasSantus.png"}
+              rating={4}
+              description={"São Paulo"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
