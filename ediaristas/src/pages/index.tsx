57aabf9bce66faad19f6e8dea111a83fc0c8b90a
Lucas Santus <leos9877@gmail.com>
2021-06-14 16:11:08 -0300

    implementando front-end do projeto

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
-import TextField from "ui/components/inputs/TextField/TextField"
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask"
+import { Button, Typography } from '@material-ui/core';
+import { FormElementsContainer } from 'ui/styles/pages/index.style';
 
 export default function Home() {
   return (
@@ -13,9 +15,25 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais em sua localidade"
         }
       />
+      <FormElementsContainer>
+        <TextFieldMask 
+          mask={'99.999-999'}
+          label={'Digite seu CEP:'}
+          fullWidth
+          variant={'outlined'}
+        />
 
-      <TextField />
-      
+        <Typography color={'error'}>CEP Inválido</Typography>
+        
+        <Button
+          variant={'contained'}
+          color={'secondary'}
+          sx={{ width: '220px' }}
+        >
+          Buscar
+        </Button>
+      </FormElementsContainer>
+
       <UserInformation
         name={"Lucas Santus"}
         picture={"https://github.com/lucasSantus.png"}
